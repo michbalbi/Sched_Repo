c3b745d853954adc75faa3253c7dca2518157c76
michbalbi <michbalbi@users.noreply.github.com>
2015-07-13 15:36:02 -0500

    First commit without files obtained after compilation.

---
+++ Sched_Project/src/Sch_Cfg.c
@@ -0,0 +1,104 @@
+/*============================================================================*/
+/*                        SV C CE SOFTWARE GROUP                              */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*
+* C Source:         %Sch_Cfg.c%
+* Instance:         1
+* %version:         1 %
+* %created_by:      Michele Balbi %
+* %date_created:    July 13 2015 %
+*=============================================================================*/
+/* DESCRIPTION : C source code for the scheduler's task configuration table.  */
+/*============================================================================*/
+/* FUNCTION COMMENT : This file describes the C source used in the scheduler's*/
+/* configuration table.                                                       */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*  REVISION |   DATE      |                               |      AUTHOR      */
+/*----------------------------------------------------------------------------*/
+/*  1.0      | 13/07/2015  |                               | Michele Balbi    */
+/* 	First draft.                                                              */
+/*============================================================================*/
+
+/* Includes */
+/* -------- */
+#include "Sch_Cfg.h"
+
+/* Functions macros, constants, types and datas         */
+/* ---------------------------------------------------- */
+/* Functions macros */
+
+/*==================================================*/ 
+/* Definition of constants                          */
+/*==================================================*/ 
+/* BYTE constants */
+
+
+/* WORD constants */
+
+
+/* LONG and STRUCTURE constants */
+
+
+
+/*======================================================*/ 
+/* Definition of RAM variables                          */
+/*======================================================*/ 
+/* BYTE RAM variables */
+
+
+/* WORD RAM variables */
+
+
+/* LONG and STRUCTURE RAM variables */
+
+
+/*======================================================*/ 
+/* close variable declaration sections                  */
+/*======================================================*/ 
+
+/* Private defines */
+
+
+/* Private functions prototypes */
+/* ---------------------------- */
+
+
+
+/* Exported functions prototypes */
+/* ----------------------------- */
+
+/* Inline functions */
+/* ---------------- */
+/**************************************************************
+ *  Name                 : inline_func	2
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+
+
+/* Private functions */
+/* ----------------- */
+/**************************************************************
+ *  Name                 : private_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+
+
+/* Exported functions */
+/* ------------------ */
+/**************************************************************
+ *  Name                 :	export_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+
---
+++ Sched_Project/src/main.c
@@ -0,0 +1,142 @@
+/*============================================================================*/
+/*                        SV C CE SOFTWARE GROUP                              */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*
+* C Source:         %main.c%
+* Instance:         1
+* %version:         1 %
+* %created_by:      Michele Balbi %
+* %date_created:    July 13 2015 %
+*=============================================================================*/
+/* DESCRIPTION : C source template file for the scheduler application.        */
+/*============================================================================*/
+/* FUNCTION COMMENT : This file describes the C source code which prepares    */
+/* the environment for the execution of the scheduler.                        */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*  REVISION |   DATE      |                               |      AUTHOR      */
+/*----------------------------------------------------------------------------*/
+/*  1.0      | DD/MM/2015  |                               | Michele Balbi    */
+/* 	First draft of the file. Includes used libraries.                         */
+/*============================================================================*/
+
+/* Includes */
+/* -------- */
+#include "MPC5606B.h"
+#include "conti_typedefs.h"
+#include "MPC5606B_GPIO_lib.h"
+#include "MPC5606B_ClkInit.h"
+#include "MPC5606B_INTERRUPTS_lib.h"
+#include "MPC5606B_PIT_lib.h"
+
+/* Functions macros, constants, types and datas         */
+/* ---------------------------------------------------- */
+/* Functions macros */
+
+/*==================================================*/ 
+/* Definition of constants                          */
+/*==================================================*/ 
+/* BYTE constants */
+
+
+/* WORD constants */
+
+
+/* LONG and STRUCTURE constants */
+
+
+
+/*======================================================*/ 
+/* Definition of RAM variables                          */
+/*======================================================*/ 
+/* BYTE RAM variables */
+
+
+/* WORD RAM variables */
+
+
+/* LONG and STRUCTURE RAM variables */
+
+
+/*======================================================*/ 
+/* close variable declaration sections                  */
+/*======================================================*/ 
+
+/* Private defines */
+
+
+/* Private functions prototypes */
+/* ---------------------------- */
+
+
+
+/* Exported functions prototypes */
+/* ----------------------------- */
+
+/* Inline functions */
+/* ---------------- */
+/**************************************************************
+ *  Name                 : inline_func	2
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+
+
+/* Private functions */
+/* ----------------- */
+/**************************************************************
+ *  Name                 : private_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+
+
+/* Exported functions */
+/* ------------------ */
+/**************************************************************
+ *  Name                 :	export_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+
+
+
+/* ---------------- MAIN FUNCTION -------------------------- */
+/**************************************************************
+ *  Name                 :	main
+ *  Description          :
+ *  Parameters           :	void
+ *  Return               :	void
+ *  Critical/explanation :  The main routine initializes the
+ 							peripherals used in this project.
+ **************************************************************/
+int main(void) {
+	
+	initModesAndClock();    /* Initialize mode entries and system clock */
+	disableWatchdog();      /* Disable watchdog */
+	initPeriClkGen();       /* Initialize peripheral clock generation for DSPIs */
+	
+	/* Interrupts init, SW Mode */
+    INT_SW_VECTOR_MODE();
+    
+    TimersInit();
+    
+    INTC_InstallINTCInterruptHandler(Timer10ms_INT_handler, PIT0_Vector, PRIORITY13);
+    
+    INT_LOWER_CPR(PRIORITY0);
+    INTC_InitINTCInterrupts();  
+	
+	
+	for (;;) {
+
+	}
+}
+
